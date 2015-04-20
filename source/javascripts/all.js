065ee1f0e827192641cc92eee8dace4a473680e4
Erica <garciadesign.me@gmail.com>
2015-04-20 10:23:33 -0500

    Felipes Blog

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,31 @@
 //= require_tree .
+
+var SideComments = require('side-comments');
+
+sideComments = new SideComments('#single-article', {}, []);
+
+// Listen to "commentPosted", and send a request to your backend to save the comment.
+// More about this event in the "docs" section.
+sideComments.on('commentPosted', function( comment ) {
+    $.ajax({
+        url: '/comments',
+        type: 'POST',
+        data: comment,
+        success: function( savedComment ) {
+            // Once the comment is saved, you can insert the comment into the comment stream with "insertComment(comment)".
+            sideComments.insertComment(comment);
+        }
+    });
+});
+ 
+// Listen to "commentDeleted" and send a request to your backend to delete the comment.
+// More about this event in the "docs" section.
+sideComments.on('commentDeleted', function( commentId ) {
+    $.ajax({
+        url: '/comments/' + commentId,
+        type: 'DELETE',
+        success: function( success ) {
+            // Do something.
+        }
+    });
+});
